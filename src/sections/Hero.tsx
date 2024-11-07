94851d30388c18a0b5f5c9941dd965412dcd9d92
RISSCoding <farisfebrian0602@gmail.com>
2024-11-07 09:24:06 +0700

    update

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -9,7 +9,7 @@ interface HeroSectionProps {
 
 export const HeroSection: React.FC<HeroSectionProps> = ({ }) => {
   return (
-    <div className="mb-[15vh]">
+    <div className="mb-[16vh]">
       <div className="mt-20 text-white mx-4 sm:mx-8 md:mx-16 lg:mx-24 xl:mx-32 h-auto flex flex-col lg:flex-row">
         <div 
           className="w-[200px] h-[300px] sm:w-[250px] sm:h-[350px] md:w-[500px] md:h-[400px] lg:w-[250px] lg:h-[445px] rounded-[30px] sm:rounded-[45px] bg-cover bg-center mb-5 lg:mb-0 mx-auto"
--- vite.config.ts
+++ vite.config.ts
@@ -11,9 +11,4 @@ export default defineConfig({
       '@': resolve(__dirname, 'src'),
     },
   },
-  server: {
-    proxy: {
-      '/api': 'http://localhost:5000',
-    },
-  },
 });
